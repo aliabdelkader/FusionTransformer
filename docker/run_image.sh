12fb1d6c2271eaba61dbd60daeef29a41db628ea
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2021-05-14 22:16:07 +0200

    update logs path

--- docker/run_image.sh
+++ docker/run_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v `pwd`/../:/home/user/FusionTransformer -v `pwd`/../../SemanticKitti:/home/user/SemanticKitti -v ~/logs:~/logs --network host  --ipc=host --runtime nvidia --gpus all --name fusion_container fusion_image
+docker run -it --rm -v `pwd`/../:/home/user/FusionTransformer -v `pwd`/../../SemanticKitti:/home/user/SemanticKitti -v `pwd`/../../logs:/home/user/logs --network host  --ipc=host --runtime nvidia --gpus all --name fusion_container fusion_image
