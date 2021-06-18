297331d2fbac9e18229b20dc2fef7a4775253c40
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2021-06-18 17:17:36 +0200

    add lidar only model

---
+++ FusionTransformer/models/lidar_model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+from FusionTransformer.models.spvcnn import SPVCNN
+
+class LidarSeg():
+    def __init__(self, num_class, backbone_3d_kwargs):
+        super(LidarSeg, self).__init__()
+
+        self.backbone = SPVCNN(**backbone_3d_kwargs)
+        
+        # segmentation head
+        self.linear = nn.Linear(self.backbone.cs[-1], num_classes)
+
+        
+    def forward(self, data_dict):
+        feats = self.backbone(data_dict["lidar"])
+        x = self.linear(feats)
+
+        preds = {
+            'lidar_feats': feats,
+            'lidar_seg_logit': x,
+        }
+
+        return preds
