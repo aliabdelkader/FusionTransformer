e645e52701289c357fefd2a90c8bcdd3fc98f29c
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2021-06-22 06:48:46 +0200

    add image, lidar, fusion usage flags

---
+++ FusionTransformer/models/image_model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from FusionTransformer.models.transformer2D import Net2DSeg
+
+class ImageSeg(nn.Module):
+    def __init__(self, num_class, dual_head, backbone_2d_kwargs):
+        super(ImageSeg, self).__init__()
+        self.image_backbone = Net2DSeg(
+                 num_classes=num_class,
+                 dual_head=dual_head,
+                 backbone_2d_kwargs=backbone_2d_kwargs)
+    
+    def forward(self, data_dict):
+        preds_image = self.image_backbone(data_dict["img"], data_dict["img_indices"])
+        return preds_image
