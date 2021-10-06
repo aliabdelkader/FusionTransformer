fix image normalization

FusionTransformer/data/semantic_kitti/semantic_kitti_dataloader.py
@@ -164,7 +164,7 @@ class SemanticKITTISCN(SemanticKITTIBase):
         keep_idx = np.ones(len(points), dtype=np.bool)
         points_img = data_dict['points_img'].copy()
         img_path = osp.join(self.semantic_kitti_dir, data_dict['camera_path'])
-        image = Image.open(img_path).crop((0, 0, self.image_width, self.image_height)) / 255
+        image = Image.open(img_path).crop((0, 0, self.image_width, self.image_height))
 
         if self.bottom_crop is not None:
             # self.bottom_crop is a tuple (crop_width, crop_height)
@@ -196,7 +196,7 @@ class SemanticKITTISCN(SemanticKITTIBase):
         if self.color_jitter is not None:
             image = self.color_jitter(image)
         # PIL to numpy
-        image = np.array(image, dtype=np.float32, copy=False)
+        image = np.array(image, dtype=np.float32, copy=False) / 255
         # 2D augmentation
         if (self.fliplr is not None) and (np.random.rand() < self.fliplr):
             image = np.ascontiguousarray(np.fliplr(image))