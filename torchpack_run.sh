f8333a6caf57a237529e949895979e5a092c9c94
Ali Abdelkader <aly.abdelkader@aucegypt.edu>
2021-12-29 12:54:20 +0200

    modify run script

--- torchpack_run.sh
+++ torchpack_run.sh
@@ -1,3 +1,7 @@
+export cwd=$(pwd)
+cd ../torchsparse-1.1.0
+python setup.py install
+cd ${cwd}
 pip install -e .
 wandb login
 torchpack dist-run -np 2 python FusionTransformer/train.py --cfg configs/semantic_kitti/lidar.yaml --use_torchpack 1
